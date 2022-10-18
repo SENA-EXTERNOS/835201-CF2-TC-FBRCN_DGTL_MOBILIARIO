13746f699b796a89de028bbf652c3fffc364afc4
Fabian_Nuñez <31360808+fabnupi23@users.noreply.github.com>
2022-10-17 21:07:27 -0500

    Tema 7
    Tema 7 OK

--- src/views/Tema7.vue
+++ src/views/Tema7.vue
@@ -5,7 +5,15 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 7
-      h1 Titulo 
+      h1 Calidad en diseño, fabricación y mantenimiento de mobiliario
+
+    p.md-5 Revisemos un elemento clave a la hora de iniciar un proceso de diseño como lo es el mantenimiento. Este es el componente en un proceso de diseño que le permite al diseñador, pensar o considerar los puntos de vista de los clientes o usuarios al momento de tomar la decisión de compra y establecer el tiempo de vida del producto. Observe más al respecto en el siguiente video. 
+
+    .row.mt-5.md-5
+      .col-12
+        figure
+          .video
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen) 
   
   
   
